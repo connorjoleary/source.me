e34617e6197aa77ff53c00c2194d88aa3be5c6da
connorjoleary <oleary.connor24@gmail.com>
2023-08-06 16:25:50 -0700

    isort and black

--- src/search/google_search.py
+++ src/search/google_search.py
@@ -1,8 +1,7 @@
-from dotenv import load_dotenv
 import os
-
 import pprint
 
+from dotenv import load_dotenv
 from googleapiclient.discovery import build
 
 load_dotenv("./keys.env")
@@ -19,7 +18,8 @@ def run_search(query: str):
     # https://developers.google.com/custom-search/v1/reference/rest/v1/cse/list
     res = service.cse().list(q=query, cx=SEARCH_ENGINE, num=3).execute()
     filtered_res = [
-        {"title": i["title"], "link": i["displayLink"], "snippet": i["snippet"]} for i in res["items"]
+        {"title": i["title"], "link": i["displayLink"], "snippet": i["snippet"]}
+        for i in res["items"]
     ]
     return filtered_res
 
