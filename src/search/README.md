26f8a16709deff26ca5e52d4b9de6679b44ea5f1
connorjoleary <oleary.connor24@gmail.com>
2023-08-06 16:14:35 -0700

    setup

---
+++ src/search/README.md
@@ -0,0 +1,4 @@
+curl \
+  'https://customsearch.googleapis.com/customsearch/v1?cx=***REMOVED***&q=dogs&key=AIzaSyCHdOrJYKjjTY0h0N0WZ8gX0cRnKye8aOs' \
+  --header 'Accept: application/json' \
+  --compressed
---
+++ src/search/google_search.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv
+import os
+
+import pprint
+
+from googleapiclient.discovery import build
+
+load_dotenv("./keys.env")
+CUSTOM_SEARCH = os.getenv("CUSTOM_SEARCH")
+SEARCH_ENGINE = os.getenv("SEARCH_ENGINE")
+
+
+def run_search(query: str):
+    # Build a service object for interacting with the API. Visit
+    # the Google APIs Console <http://code.google.com/apis/console>
+    # to get an API key for your own application.
+    service = build("customsearch", "v1", developerKey=CUSTOM_SEARCH)
+
+    # https://developers.google.com/custom-search/v1/reference/rest/v1/cse/list
+    res = service.cse().list(q=query, cx=SEARCH_ENGINE, num=3).execute()
+    filtered_res = [
+        {"title": i["title"], "link": i["displayLink"], "snippet": i["snippet"]}
+        for i in res["items"]
+    ]
+    return filtered_res
+
+
+res = run_search("Silver in blood causes people to turn blue.")
+pprint.pprint(res)
